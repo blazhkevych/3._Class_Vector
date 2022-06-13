9c9281ed63485a2ffb22d37924fb1fee39824ffe
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-06-13 16:07:49 +0300

    Add project files.

---
+++ task/Vector.cpp
@@ -0,0 +1,134 @@
+﻿#include <stdlib.h>
+#include <iostream>
+#include "Vector.h"
+using std::cout;
+using std::endl;
+
+// Конструктор по умолчанию
+Vector::Vector()
+{
+	m_vect = nullptr;
+	m_size = 0;
+}
+
+// Конструктор, позволяющий задавать размерность вектора
+Vector::Vector(int size)
+{
+	m_size = size;
+	m_vect = new int[m_size];
+	int min = 0; // Минимальное значение
+	int max = 100; // Максимальное значение
+	for (int i = 0; i < size; i++) // Заполняем одномерный массив псевдослучайными числами.
+		m_vect[i] = rand() % (max - min + 1) + min;
+}
+
+// Конструктор, позволяющий проинициализировать вектор с помощью существующего массива
+Vector::Vector(const int* ptr, int size)
+{
+	m_size = size;
+	m_vect = new int[m_size];
+	for (int i = 0; i < m_size; i++)
+		m_vect[i] = ptr[i];
+}
+
+// Конструктор копирования
+Vector::Vector(const Vector& v)
+{
+
+}
+
+// Деструктор
+Vector::~Vector()
+{
+	delete[] m_vect;
+}
+
+// Функция возвращает размерность вектора
+int Vector::GetSize() const
+{
+	return m_size;
+}
+
+// Вывод на экран
+void Vector::Print() const
+{
+	for (int i = 0; i < m_size; i++)
+		cout << m_vect[i] << '\t';
+	cout << endl;
+}
+
+// Заполнение вектора
+void Vector::Input()
+{
+	int min = 0; // Минимальное значение
+	int max = 100; // Максимальное значение
+	if (m_size != 0)
+		for (int i = 0; i < m_size; i++)
+			m_vect[i] = rand() % (max - min + 1) + min; // Заполнение одномерного массива псевдослучайными числами.
+}
+
+// Очистка вектора
+void Vector::Clear()
+{
+	for (int i = 0; i < m_size; i++)
+		m_vect[i] = 0;
+}
+
+// Возвращает истину, если вектор пустой
+bool Vector::IsEmpty() const
+{
+	return m_size <= 0 ? true : false;
+}
+
+// Добавление элемента в вектор
+void Vector::Add(const int& item)
+{
+	int* p = new int[++m_size];
+	if (m_vect != nullptr)
+		for (int i = 0; i < m_size - 1; i++)
+			p[i] = m_vect[i];
+	p[m_size - 1] = item;
+	delete[]m_vect;
+	m_vect = p;
+	p = nullptr;
+}
+
+// Вставка элемента в вектор по заданному индексу
+void Vector::Insert(int index, const int& item)
+{
+	if (index < 0 || index > m_size - 1)
+		return;
+	int* p = new int[++m_size];
+	int k = 0;
+	for (int i = 0; i < m_size; i++)
+	{
+		if (index == i)
+		{
+			k = 1;
+			p[i] = item;
+		}
+		else
+			p[i] = m_vect[i - k];
+	}
+	delete[]m_vect;
+	m_vect = p;
+	p = nullptr;
+}
+
+// Удаление элемента из вектора по заданному индексу
+void Vector::Remove(int index)
+{
+	if (index < 0 || index > m_size - 1)
+		return;
+	int* p = new int[--m_size];
+	int k = 0;
+	for (int i = 0; i < m_size; i++)
+	{
+		if (index == i)
+			k = 1;
+		p[i] = m_vect[i + k];
+	}
+	delete[]m_vect;
+	m_vect = p;
+	p = nullptr;
+}
---
+++ task/task.cpp
@@ -0,0 +1,74 @@
+﻿/*
+	СТАТУС: Не готово
+
+	ИЗВЕСТНЫЕ ОШИБКИ:
+	// Добавление элемента в вектор
+	функция сбоит при работе с вектором созданным конструктором по умолчанию
+
+
+
+	ВИДЕО: 2:43:52
+*/
+#include <iostream>
+#include "Vector.h"
+using std::cout;
+using std::endl;
+using std::boolalpha;
+
+int main()
+{
+	srand(time(0));
+	int size{ 5 };
+	int* arr = new int[size] {1, 2, 3, 4, 5};
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << ' ';
+	}
+	cout << endl;
+
+	Vector v1;
+	Vector v2{ 3 };
+	Vector v3{ arr, size };
+
+	cout << "\nv1 size is " << v1.GetSize() << endl;
+	cout << "\nv2 size is " << v2.GetSize() << endl;
+	cout << "\nv3 size is " << v3.GetSize() << endl;
+
+	v1.Print();
+	v2.Print();
+	v3.Print();
+
+	v2.Add(999);
+	v2.Print();
+
+	v2.Add(888);
+	v2.Print();
+
+	v2.Add(777);
+	v2.Print();
+
+	v2.Clear();
+	v2.Print();
+	v2.Input();
+	v2.Print();
+
+	cout << "\nv1 IsEmpty - " << boolalpha << v1.IsEmpty() << endl;
+	cout << "\nv2 IsEmpty - " << boolalpha << v2.IsEmpty() << endl;
+	cout << "\nv3 IsEmpty - " << boolalpha << v3.IsEmpty() << endl;
+
+	v2.Print();
+	cout << "\nv2 size is " << v2.GetSize() << endl;
+	v2.Insert(2, 999999);
+	v2.Print();
+	cout << "\nv2 size is " << v2.GetSize() << endl;
+
+	v2.Add(777777);
+	v2.Print();
+	cout << "\nv2 size is " << v2.GetSize() << endl;
+
+	v2.Remove(2);
+	v2.Print();
+	cout << "\nv2 size is " << v2.GetSize() << endl;
+
+	delete[]arr;
+}
